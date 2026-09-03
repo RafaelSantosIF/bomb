439fc758aca5b5205b2abee4e01c626e361f47ec
RafaelSantosIF <rafael.santos61@aluno.ifce.edu.br>
2026-09-03 16:32:13 -0300

    theme

---
+++ src/constants/theme.ts
@@ -0,0 +1,16 @@
+export const colors = {
+    bg: '#101114',
+    surface: '#16181D',
+    surface2: '#20242A',
+    border: '#23272E',
+    acc: '#A3E635',
+    text: '#FAFAFA',
+    text2: '#A1A1AA',
+    control: '#1B1F25',
+    buy: '#141712',
+    remove: '#F87171',
+    removeBg: '#241417',
+};
+
+export const spacing = { xs: 6, sm: 10, md: 14, lg: 16, xl: 22 };
+
